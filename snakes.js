d95b153fc3611152ca6d23bc3bd310000ab1d56a
Sean Darsie <sdarsie@Seans-iMac.local>
2017-11-14 09:04:30 -0800

    plus minus food rate

--- snakes.js
+++ snakes.js
@@ -3,6 +3,9 @@ var KEYDOWN = 38;
 var KEYRIGHT = 39;
 var KEYUP = 40;
 var SPACE = 32;
+var ESC = 27;
+var MINUS = 189;
+var PLUS = 187;
 
 var player = class player {
     constructor(posx, posy, tail, alive, dirx, diry, score) {
@@ -32,6 +35,7 @@ var trail = [];
 var foodColor = "red"
 var snakeColor = "lime";
 var speed = 50;
+var foodRate = 20;
 
 window.onload=function() {
     trailLength = document.getElementById("snakeLength");
@@ -63,7 +67,7 @@ function game() {
     context.fillRect(0,0,canvas.width, canvas.height);
     foodSpawn += 1;
     // console.log(foodSpawn);
-    if (foodSpawn >= 20) {
+    if (foodSpawn >= foodRate) {
         foods.push({x: Math.floor(Math.random() * tileCount), y: Math.floor(Math.random() * tileCount)});
         foodSpawn = 0;
     }
@@ -74,6 +78,8 @@ function game() {
         if (trail[i].x == player.posx && trail[i].y == player.posy) {
             player.tail = 4;
             player.score = 0;
+            player.diry = 0;
+            player.dirx = 0;
             // speed = 1000 / 15;
 
         }
@@ -129,6 +135,22 @@ function getSnakeColor() {
 function keyPush(e) {
     console.log(e.keyCode);
     switch(e.keyCode) {
+        // case ESC:
+        //     var playerTail = player.tail;
+        //     player.tail = 0;
+        //     player.diry = 0;
+        //     player.dirx = 0;
+        //     player.tail = playerTail;
+        //     break;
+        case PLUS:
+            foodRate -= 1;
+            if (foodRate < 5) {
+                foodRate = 5;
+            }
+            break;
+        case MINUS:
+            foodRate += 1;
+            break;
         case SPACE:
             speed = 40;
             setInterval(game, speed);
